78dde50c5fac484c5fdc0bc5631860927bc88f19
davidtav <david.mclaurel@outlook.com.br>
2025-01-03 20:52:59 -0300

    conceitos iniciais de Controller,Routes,Views e comando php artisan make

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Welcome to Laravel 11";
 });
+Route::get('/about', function () {
+    echo "About us";
+});
+
+Route::get('/main',[MainController::class,'index']);

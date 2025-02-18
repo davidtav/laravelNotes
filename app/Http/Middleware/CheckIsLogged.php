3d58469e7ad4690cde5f139055aaf0c4b2358571
davidtav <david.mclaurel@outlook.com.br>
2025-02-18 13:53:24 -0300

    criação de midllewares para controle de acesso

---
+++ app/Http/Middleware/CheckIsLogged.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIsLogged
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Se o usuário não estiver logado, redireciona para a página de login
+        if (!session()->has('user')) {
+            return redirect('/login');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-//auth routes
+//os middlewares abaixo verificam o status do usuário, para mais informações veja a pasta Middleware
+
+Route::middleware([CheckIsNotLogged::class])->group(function(){
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware( [CheckIsLogged::class])->group(function (): void {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+

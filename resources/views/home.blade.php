bc5b2b288890624a2e7f61315b5a9738ef6dac57
davidtav <david.mclaurel@outlook.com.br>
2025-02-24 20:48:02 -0300

    componente de navegação da aplicação

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,30 +2,8 @@
 @section('content')
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col">
-
-                <div class="row mb-3 align-items-center">
-                    <div class="col">
-                        <a href="{{route('home')}} ">
-                            <img src="assets/images/logo.png" alt="Notes logo">
-                        </a>
-                    </div>
-                    <div class="col text-center">
-                        Aprendendo <span class="text-warning">Laravel</span> com projetos!
-                    </div>
-                    <div class="col">
-                        <div class="d-flex justify-content-end align-items-center">
-                            <span class="me-3"><i
-                                    class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>[username]</span>
-                            <a href="{{route('logout')}}" class="btn btn-outline-secondary px-3">
-                                Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <hr>
-
+            <div class="col">  
+                @include('top_bar')         
                 <!-- no notes available -->
                 <div class="row mt-5">
                     <div class="col text-center">

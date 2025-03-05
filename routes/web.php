f9a30e66e1f5460d795d5e7d3b1b65ff1c9613f9
davidtav <david.mclaurel@outlook.com.br>
2025-03-05 00:32:33 -0300

    rota de edição da e uma nota

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::middleware( [CheckIsLogged::class])->group(function (): void {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+
+    //rota para editar uma nota
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 

6c7763ebc1c2e5ca2303c092d0cd4a1ade98ff9a
davidtav <david.mclaurel@outlook.com.br>
2025-04-05 20:35:16 -0300

    criação de um service para otimizar o processo de decriptação

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -28,22 +29,12 @@ class MainController extends Controller
   }
   public function editNote($id)
   {
-    $id = $this->decryptId($id);
+    $id = Operations::decryptId($id);
     echo "I'm editing a note with id: $id";
   }
     public function deleteNote($id)
     {
-      $id = $this->decryptId($id);
+      $id = Operations::decryptId($id);
       echo "I'm deleting a note with id: $id";
-    }
-
-    private function decryptId($id)
-    { 
-      try {
-        $id = Crypt::decrypt($id);
-      } catch (DecryptException $e) {
-        return redirect()->route('home');
-      }
-      return $id;
     }
 }
